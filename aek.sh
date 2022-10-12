8ee5acf7f2e0cf477654899d2d5fa0005a1d587a
faraz <33409348+bixBeta@users.noreply.github.com>
2022-10-12 13:43:28 -0400

    init

--- aek.sh
+++
@@ -1,6 +0,0 @@
-/programs/sambamba-0.6.7/sambamba \
-view -h -t 8 -s 0.1 -f bam --subsampling-seed=1 \
-BC1-L4LC_GTGAAA_R1_trimmed.fq.gz.Aligned.sortedByCoord.out.bam -o subSamp.bam
-
-
-FACTOR=$(samtools idxstats $1 | cut -f3 | awk -v COUNT=$2 'BEGIN {total=0} {total += $1} END {print COUNT/total}')
